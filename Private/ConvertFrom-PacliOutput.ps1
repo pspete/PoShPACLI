deea98def9c70b2a890e85f5083c996be5269bbf
Pete Maan <pete.maan+github@gmail.com>
2017-08-22 00:09:01 +0100

    :ok_hand: FIX OutputType
    Set OutputType as per PSScriptAnalyzer recommendation

--- Private/ConvertFrom-PacliOutput.ps1
+++ Private/ConvertFrom-PacliOutput.ps1
@@ -48,7 +48,7 @@
     #>
 
 	[CmdLetBinding()]
-	[OutputType('System.Array')]
+	[OutputType('System.Object[]')]
 	param(
 		[Parameter(Mandatory = $True, ValueFromPipeline = $True)][string]$pacliOutput,
 		[Parameter(Mandatory = $False, ValueFromPipeline = $False)][string]$regEx = '"([^"]*)"'
