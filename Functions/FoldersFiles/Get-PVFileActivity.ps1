0da72565a4573e1d3b39c5f78121757c6527718c
Pete Maan <pete.maan+github@gmail.com>
2017-08-26 00:20:07 +0100

    UPDATE Output
    Add View for Activity
    Update typename on output object

--- Functions/FoldersFiles/Get-PVFileActivity.ps1
+++ Functions/FoldersFiles/Get-PVFileActivity.ps1
@@ -95,7 +95,7 @@
 						"RequestReason"    = $values[5]
 						"Code"             = $values[6]
 
-					} | Add-ObjectDetail -TypeName pacli.PoShPACLI -PropertyToAdd @{
+					} | Add-ObjectDetail -TypeName pacli.PoShPACLI.Activity -PropertyToAdd @{
 						"vault"     = $vault
 						"user"      = $user
 						"sessionID" = $sessionID
