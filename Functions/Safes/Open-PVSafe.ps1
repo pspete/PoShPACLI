57eb094d8599c3d5e667a2f5d3aca8ed48102701
Pete Maan <pete.maan+github@gmail.com>
2017-08-25 17:22:34 +0100

    UPDATE Output
    Add custom TypeName to Output object

--- Functions/Safes/Open-PVSafe.ps1
+++ Functions/Safes/Open-PVSafe.ps1
@@ -142,11 +142,11 @@
 					"RequireReason"             = $Results[12]
 					"EnforceExclusivePasswords" = $Results[13]
 
-				} | Add-ObjectDetail -TypeName pacli.PoShPACLI -PropertyToAdd @{
-						"vault"     = $vault
-						"user"      = $user
-						"sessionID" = $sessionID
-					}
+				} | Add-ObjectDetail -TypeName pacli.PoShPACLI.Safe -PropertyToAdd @{
+					"vault"     = $vault
+					"user"      = $user
+					"sessionID" = $sessionID
+				}
 
 			}
 
