4940f9cb88f4016355eb2e904a8d16596840a1c3
Pete Maan <pete.maan+github@gmail.com>
2017-09-07 22:10:51 +0100

    ADD Output
    Default properties output

--- Functions/SessionManagement/Disconnect-PVVault.ps1
+++ Functions/SessionManagement/Disconnect-PVVault.ps1
@@ -67,6 +67,14 @@
 
 				Write-Verbose "Successfully Logged Off"
 
+				[PSCustomObject] @{
+
+					"vault"     = $vault
+					"user"      = $user
+					"sessionID" = $sessionID
+
+				} | Add-ObjectDetail -TypeName pacli.PoShPACLI
+
 			}
 
 		}
