22b5e6d6968ca11f89e46d8f765713db8ac1852c
Pete Maan <pete.maan+github@gmail.com>
2019-03-06 13:45:46 +0000

    :construction_worker: Add CI Helpers
    General CI Install, Build, Test, Deploy workflow for Appveyor, Coveralls and PowerShell Gallery workflow

---
+++ build/test.ps1
@@ -0,0 +1,37 @@
+#---------------------------------#
+# Header                          #
+#---------------------------------#
+Write-Host "Testing:" -ForegroundColor Yellow
+Write-Host "Current working directory: $pwd"
+
+#---------------------------------#
+# Run Pester Tests                #
+#---------------------------------#
+$files = Get-ChildItem ".\VaultControl" -Include *.ps1 -Recurse
+
+$res = Invoke-Pester -Path ".\Tests" -OutputFormat NUnitXml -OutputFile TestsResults.xml -CodeCoverage $files -PassThru
+
+Write-Host 'Uploading Test Results'
+(New-Object 'System.Net.WebClient').UploadFile("https://ci.appveyor.com/api/testresults/nunit/$($env:APPVEYOR_JOB_ID)", (Resolve-Path .\TestsResults.xml))
+
+if($env:APPVEYOR_REPO_COMMIT_AUTHOR -eq "Pete Maan") {
+
+	Write-Host 'Formating Code Coverage'
+	$coverage = Format-Coverage -PesterResults $res -CoverallsApiToken $($env:coveralls_key) -BranchName $($env:APPVEYOR_REPO_BRANCH)
+
+	Write-Host 'Publishing Code Coverage'
+	Publish-Coverage -Coverage $coverage
+
+}
+#---------------------------------#
+# Validate                        #
+#---------------------------------#
+if (($res.FailedCount -gt 0) -or ($res.PassedCount -eq 0)) {
+
+	throw "$($res.FailedCount) tests failed."
+
+} else {
+
+	Write-Host 'All tests passed' -ForegroundColor Green
+
+}
