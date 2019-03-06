502d2289d83885beedd1093a375c20ada1c2888c
Pete Maan <pete.maan+github@gmail.com>
2019-03-06 16:05:50 +0000

    :hammer: UPDATE PoShPACLI.psm1
    Update to improve module import time.
    Also added logic to allow import of persistent config data in preparation for planned module update.

--- PoShPACLI/PoShPACLI.psm1
+++ PoShPACLI/PoShPACLI.psm1
@@ -15,28 +15,42 @@
 
 #>
 [CmdletBinding()]
-param()
+param(
+
+	[bool]$DotSourceModule = $false
+
+)
 
 #Get function files
-Write-Verbose $PSScriptRoot
-
-Get-ChildItem $PSScriptRoot\ -Recurse -Filter "*.ps1" -Exclude "*.ps1xml" |
+Get-ChildItem $PSScriptRoot\ -Recurse -Include "*.ps1" |
 
 ForEach-Object {
 
-	Try {
-		# Dot Source each file
-		. (
-			[scriptblock]::Create(
-				[io.file]::ReadAllText($_.fullname, [Text.Encoding]::UTF8)
-			)
+	if ($DotSourceModule) {
+		. $_.FullName
+	} else {
+		$ExecutionContext.InvokeCommand.InvokeScript(
+			$false,
+			(
+				[scriptblock]::Create(
+					[io.file]::ReadAllText(
+						$_.FullName,
+						[Text.Encoding]::UTF8
+					)
+				)
+			),
+			$null,
+			$null
 		)
-
-	} Catch {
-
-		Write-Error "Failed to import function $($_.fullname)"
 
 	}
 
+}
 
+#Read config and make available in script scope
+$ConfigFile = "$env:HOMEDRIVE$env:HomePath\PoShPACLIConfiguration.xml"
+If(Test-Path $ConfigFile) {
+	Write-Verbose "Importing Settings: $ConfigFile"
+	$config = Import-Clixml -Path $ConfigFile
+	Set-Variable -Name PoShPACLI -Value $config -Scope Script
 }
