131b1382ef3ec400230feb2d82799a1ffa8687c7
Pete Maan <pete.maan+github@gmail.com>
2019-03-06 13:43:32 +0000

    :truck: Restructure Module
    Moved module files to .\PoSHPACLI directory

---
+++ PoShPACLI/PoShPACLI.psm1
@@ -0,0 +1,42 @@
+﻿<#
+.SYNOPSIS
+
+.DESCRIPTION
+
+.EXAMPLE
+
+.INPUTS
+
+.OUTPUTS
+
+.NOTES
+
+.LINK
+
+#>
+[CmdletBinding()]
+param()
+
+#Get function files
+Write-Verbose $PSScriptRoot
+
+Get-ChildItem $PSScriptRoot\ -Recurse -Filter "*.ps1" -Exclude "*.ps1xml" |
+
+ForEach-Object {
+
+	Try {
+		# Dot Source each file
+		. (
+			[scriptblock]::Create(
+				[io.file]::ReadAllText($_.fullname, [Text.Encoding]::UTF8)
+			)
+		)
+
+	} Catch {
+
+		Write-Error "Failed to import function $($_.fullname)"
+
+	}
+
+
+}
