254cfe99b3256768a47ffc439d0e0d4a91c23e37
Pete Maan <pete.maan+github@gmail.com>
2019-03-10 02:19:14 +0000

    :white_check_mark: ADD Stop-PVPacli.Tests

---
+++ Tests/Stop-PVPacli.Tests.ps1
@@ -0,0 +1,76 @@
+#Get Current Directory
+$Here = Split-Path -Parent $MyInvocation.MyCommand.Path
+
+#Get Function Name
+$FunctionName = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -Replace ".Tests.ps1"
+
+#Assume ModuleName from Repository Root folder
+$ModuleName = Split-Path (Split-Path $Here -Parent) -Leaf
+
+#Resolve Path to Module Directory
+$ModulePath = Resolve-Path "$Here\..\$ModuleName"
+
+#Define Path to Module Manifest
+$ManifestPath = Join-Path "$ModulePath" "$ModuleName.psd1"
+
+#Preference file must be removed and module must be re-imported for tests to complete
+Remove-Item -Path "$env:HOMEDRIVE$env:HomePath\PV_Configuration.xml" -Force -ErrorAction SilentlyContinue
+Remove-Module -Name $ModuleName -Force -ErrorAction SilentlyContinue
+Import-Module -Name "$ManifestPath"  -ArgumentList $true -Force -ErrorAction Stop
+
+BeforeAll {
+
+	#$Script:RequestBody = $null
+
+}
+
+AfterAll {
+
+	#$Script:RequestBody = $null
+
+}
+
+Describe $FunctionName {
+
+	InModuleScope $ModuleName {
+
+		Context "Default" {
+
+			BeforeEach {
+
+				$InputObj = @{
+
+				}
+
+				Mock Invoke-PACLICommand -MockWith {
+					[PSCustomObject]@{
+						StdOut="SomeOutput"
+						ExitCode=0
+					}
+				}
+
+			}
+
+			It "executes without exception" {
+
+				{Stop-PVPacli} | Should Not throw
+
+			}
+
+			It "invokes expected pacli command" {
+
+				Stop-PVPacli
+
+				Assert-MockCalled Invoke-PACLICommand -Times 1 -Exactly -Scope It -ParameterFilter {
+
+					$PacliCommand -eq "TERM"
+
+				}
+
+			}
+
+		}
+
+	}
+
+}
