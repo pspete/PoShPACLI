756a49625658d138d786c62b8c8cd2275f7326f6
Pete Maan <pete.maan+github@gmail.com>
2019-12-30 02:02:57 +0000

    :recycle: UPDATE Stop-PVPacli
    Redirect output to $null.
    no longer remove value of `$script:PV.sessionID` on success.

--- PoShPACLI/Functions/Stop-PVPacli.ps1
+++ PoShPACLI/Functions/Stop-PVPacli.ps1
@@ -21,12 +21,6 @@
     [Diagnostics.CodeAnalysis.SuppressMessageAttribute("PSShouldProcess", "", Justification = "ShouldProcess handling is in Invoke-PACLICommand")]
     param()
 
-    $Return = Invoke-PACLICommand -PacliEXE $Script:PV.ClientPath -PacliCommand TERM -CommandParameters $($PSBoundParameters | ConvertTo-ParameterString -NoVault -NoUser)
-
-    if ($Return.ExitCode -eq 0) {
-
-        $Script:PV.PSObject.Properties.Remove('sessionID')
-
-    }
+    $Null = Invoke-PACLICommand -PacliEXE $Script:PV.ClientPath -PacliCommand TERM -CommandParameters $($PSBoundParameters | ConvertTo-ParameterString -NoVault -NoUser)
 
 }
