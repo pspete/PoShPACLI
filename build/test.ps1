c05278fc4cf9ca850db21096fd259a2e531a4713
Pete Maan <pete.maan+github@gmail.com>
2020-02-27 21:30:22 +0000

    :construction_worker: :green_heart: UPDATE Build
    Updated build & deploy process

--- build/test.ps1
+++ build/test.ps1
@@ -1,26 +1,36 @@
 #---------------------------------#
 # Header                          #
 #---------------------------------#
-Write-Host "Testing:" -ForegroundColor Yellow
-Write-Host "Current working directory: $pwd"
+Write-Host "Testing: PSVersion $($PSVersionTable.PSVersion)" -ForegroundColor Yellow
 
 #---------------------------------#
 # Run Pester Tests                #
 #---------------------------------#
-$files = Get-ChildItem ".\PoShPACLI" -Include *.ps1 -Recurse
+$files = Get-ChildItem $(Join-Path $ENV:APPVEYOR_BUILD_FOLDER $env:APPVEYOR_PROJECT_NAME) -Include *.ps1 -Recurse
 
-$res = Invoke-Pester -Path ".\Tests" -OutputFormat NUnitXml -OutputFile TestsResults.xml -CodeCoverage $files -PassThru
+$res = Invoke-Pester -Path ".\Tests" -OutputFormat NUnitXml -OutputFile TestsResults.xml -CodeCoverage $files -PassThru -Show Summary, Failed
 
 Write-Host 'Uploading Test Results'
-(New-Object 'System.Net.WebClient').UploadFile("https://ci.appveyor.com/api/testresults/nunit/$($env:APPVEYOR_JOB_ID)", (Resolve-Path .\TestsResults.xml))
+$null = (New-Object 'System.Net.WebClient').UploadFile("https://ci.appveyor.com/api/testresults/nunit/$($env:APPVEYOR_JOB_ID)", $(Resolve-Path .\TestsResults.xml))
 
-if($env:APPVEYOR_REPO_COMMIT_AUTHOR -eq "Pete Maan") {
+Remove-Item -Path $(Resolve-Path .\TestsResults.xml) -Force
+
+if ($env:APPVEYOR_REPO_COMMIT_AUTHOR -eq "Pete Maan") {
 
 	Write-Host 'Formating Code Coverage'
 	$coverage = Format-Coverage -PesterResults $res -CoverallsApiToken $($env:coveralls_key) -BranchName $($env:APPVEYOR_REPO_BRANCH)
 
+	$null = Export-CodeCovIoJson -CodeCoverage $res.CodeCoverage -RepoRoot $pwd -Path coverage.json
+
 	Write-Host 'Publishing Code Coverage'
-	Publish-Coverage -Coverage $coverage
+	$null = Publish-Coverage -Coverage $coverage
+
+	$null = Invoke-WebRequest -Uri 'https://codecov.io/bash' -OutFile codecov.sh
+
+	$null = bash codecov.sh -f coverage.json
+
+	Remove-Item -Path $(Resolve-Path .\coverage.json) -Force
+	Remove-Item -Path $(Resolve-Path .\codecov.sh) -Force
 
 }
 #---------------------------------#
@@ -30,7 +40,8 @@ if (($res.FailedCount -gt 0) -or ($res.PassedCount -eq 0)) {
 
 	throw "$($res.FailedCount) tests failed."
 
-} else {
+}
+else {
 
 	Write-Host 'All tests passed' -ForegroundColor Green
 
